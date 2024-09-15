17b2e624dd1e5c71a806db4d4e65fa74c47245be
LukeGearty <lukegearty@gmail.com>
2024-09-15 18:14:20 -0400

    worked on tasks class

--- todolist/tasks/models.py
+++ todolist/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    date_started = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
